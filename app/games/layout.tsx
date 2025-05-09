909ef465b97aa8ac65e1ca7078e4a3fb807e7a56
07CalC <maheshwarivinayak90@gmail.com>
2025-05-09 17:46:07 +0530

    bug fix

--- app/games/layout.tsx
+++ app/games/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata ={
 
 export default function GamesLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="">
             {children}
         </div>
     );
